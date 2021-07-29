02ef22a8f6ba8a677b47ae640e7e87bc822abe25
yangger <i@yangger.cn>
2021-07-29 10:16:39 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e036a36a2a67d4c119178c531de14dd1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fdeae5bc-c217-48b0-9485-3bd1a3bf4e7b'
+};
