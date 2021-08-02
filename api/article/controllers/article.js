073c29ab3f16c9d3cdbf0a736123a17b3027b866
yangger <i@yangger.cn>
2021-08-02 15:23:36 +0800

    chore: update article controller

--- api/article/controllers/article.js
+++ api/article/controllers/article.js
@@ -9,8 +9,14 @@ module.exports = {
 
   async findOne(ctx) {
     const { slug } = ctx.params;
-
     const entity = await strapi.services.article.findOne({ slug });
-    return sanitizeEntity(entity, { model: strapi.models.article });
+    const id = entity.id
+    const prevItem = await strapi.services.article.findOne({ 'id': id - 1 })
+    const nextItem = await strapi.services.article.findOne({ 'id': id + 1 })
+    return {
+      ...sanitizeEntity(entity, { model: strapi.models.article }),
+      nextItem,
+      prevItem
+    };
   },
 };
--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -9,7 +9,6 @@ module.exports = {
 
   async findOne(ctx) {
     const { slug } = ctx.params;
-
     const entity = await strapi.services.category.findOne({ slug });
     return sanitizeEntity(entity, { model: strapi.models.category });
   },
